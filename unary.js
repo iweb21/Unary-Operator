0ede87547a7d1397f00267618b5189f91ad74e6b
Sumera Imran <159164615+iweb21@users.noreply.github.com>
2024-03-17 00:02:00 +0500

    Add files via upload

---
+++ unary.js
@@ -0,0 +1,6 @@
+// HOME WORK: (Unary Operator)
+var a = 5;
+var b = 2;
+var c;
+c = ++a + a++ + --b + b-- + b++ + b;
+console.log(c);
---
+++ unary.ts
@@ -0,0 +1,9 @@
+          // HOME WORK:1(Unary Operator)
+          
+
+let a : number = 5;
+let b : number = 2;
+let c : number;
+
+c = ++a  +     a++  +  --b   +  b--   + b++   +b;
+console.log(c);
